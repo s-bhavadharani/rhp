a7634c9aa347ac2387001370521b669ce5957cea
s-bhavadharani <gnamrutha.1977@gmail.com>
2026-03-03 21:33:44 +0530

    Create ImplementingRunnable.java

---
+++ ImplementingRunnable.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        
+        for(int i=1;i<=k;i++){
+        	Worker w=new Worker(i,n);
+        	w.start();
+        }
+
+    }
+}
+
+class Worker implements Runnable{
+	int id;
+	int n;
+	public Worker(int id,int n){
+		this.id=id;
+		this.n=n;
+	}
+	@Override
+	public void run(){
+	for(int i=1;i<=n;i++){
+		System.out.println("Worker : "+id+" task : "+i);
+	}
+	}
+}
